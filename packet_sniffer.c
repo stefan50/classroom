26fda0b8621488c7489e4d4bc316dd92166ee4fb
Stefan Angelov <ste.angelov.tues19@gmail.com>
2019-05-07 09:53:56 +0300

    Create packet_sniffer.c

---
+++ packet_sniffer.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <netinet/ip_icmp.h>
+#include <linux/if_packet.h>
+#include <net/ethernet.h> /* the L2 protocols */
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+void* listen_traffic() {
+    //thread for listening
+    int socket_id = socket(PF_INET, SOCK_RAW, IPPROTO_TCP);
+    close(socket_id);
+    //pthread_exit(NULL);
+}
+
+int main() {
+    struct pthread_t* threads = (struct pthread_t*)malloc(2);
+    
+
+    //pthread_create(threads, NULL, listen_traffic, NULL);
+
+    int socket_id = socket(AF_INET, SOCK_RAW, IPPROTO_TCP); 
+    while(1) {
+        unsigned char* buffer = (unsigned char*)malloc(65536);
+        printf("message = %d\n", recv(socket_id, buffer, sizeof buffer, 0));
+        perror("Error");
+        struct iphdr* iph = (struct iphdr*) buffer;
+        struct sockaddr_in* source = (struct sockaddr_in*)malloc(65536);
+        memset(source, 0, sizeof(&source));
+        source->sin_addr.s_addr = iph->saddr;
+        printf("Source IP: %s\n", inet_ntoa(source->sin_addr));
+        struct tcphdr* tcph = (struct tcphdr*)(buffer + iph->ihl*4);
+
+        printf("Source port = %d\n", ntohs(tcph->source));
+        free(buffer);
+    }
+    //pthread_join(*threads, NULL);
+    
+    free(threads);
+    close(socket_id);
+    return 0;
+}
