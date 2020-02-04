5101e33e0757e0fad1237d6c70f6557c6b7dbd21
steangel <ste.angelov.tues19@gmail.com>
2020-02-04 12:59:53 +0200

    A very special dog :)

--- class.cpp
+++ class.cpp
@@ -1,45 +1,81 @@
 #include <iostream>
+#include <cstring>
+
 using namespace std;
 
-class Point
-{
-//private
-	double x_; //naming convention
-	double y_;
+class Dog {
+	double height_;
+	double width_;
+	bool is_alive_;
+	double age_;
+	char collar_colour_[50];
 public:
-	Point() //no arguments = default constructor
-	{
-		x_ = 6;
-		y_ = 6;
+	Dog(double height, double width, char collar_colour[50], double age) {
+		height_ = height;
+		width_ = width;
+		strcpy(collar_colour_, collar_colour);
+		age_ = age;
+		is_alive_ = true;
 	}
-	Point(double x, double y) 
-	{
-		x_ = x;
-		y_ = y;	
+
+	double get_height() {
+		return height_;
 	}
-	double get_x()
-	{
-		return x_;
+
+	double get_width() {
+		return width_;
 	}
-	double get_y()
-	{
-		return y_;
+
+	char* get_colour() {
+		return collar_colour_;
+	}
+
+	bool is_alive() {
+		return is_alive_;
+	}
+
+	double get_age() {
+		return age_;
+	}
+
+	void set_height(double height) {
+		height_ = height;
+	}
+
+	void kill() {
+		is_alive_ = false;
+		cout << "Your dog is dead!" << endl;
+	}
+
+	void feed(int ammount) {
+		age_ += ((ammount/2) + (ammount/3));
+	        if (age_ >= 10) {
+			kill();
+		}	
+	}
+
+	void feed(Dog dog) {
+		age_ -= (dog.get_age()/2);
+		cout << "You just sacrificed a dog! Shame on you!!!" << endl;
 	}
 };
 
-class Rectangle 
-{
-	Point m_;
-	Point n_;
-public:
-	
-};
-int main()
-{
-	Point mypo(2,3), yopo; //yopo = 0, 0 (default)	
-	//cout << "I have " << mypo.get_x() << ", " << mypo.get_y() << endl;
-	//cout << "You have " << yopo.get_x() << ", " << yopo.get_y() << endl;	
-	Rectangle myrec;
-	cout << myrec.get_x() <<" " << myrec.get_y() << endl;			
+int main() {
+	char tupo[50];
+	char food[50];
+	cin >> tupo;
+	Dog lilly(2,3,tupo,5);
+	while(lilly.is_alive()) {
+		cout << "Please, feed your dog: ";
+		cin >> food;
+		if(!strcmp(food, "dog")) {
+			Dog dog(2,3,tupo,5);
+			lilly.feed(dog);
+		}
+		else {
+			int foodd = atoi(food);
+		        lilly.feed(foodd);	
+		}	
+	}
 	return 0;
 }
